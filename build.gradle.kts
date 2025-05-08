2e1de5c52455a666ea66a4e0c7a82451ade16bdd
Nikolaev Dmitriy <krutaxe@mail.ru>
2025-05-08 21:03:58 +0300

    Скрипты и кастомизация в Gradle [#505353]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	checkstyle
-	java
-	jacoco
-	id("org.springframework.boot") version "3.4.0"
-	id("io.spring.dependency-management") version "1.1.6"
+    checkstyle
+    java
+    jacoco
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
     id("com.github.spotbugs") version "6.0.26"
 }
 
@@ -33,21 +33,25 @@ tasks.jacocoTestCoverageVerification {
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	compileOnly(libs.lombok)
-	annotationProcessor(libs.lombok)
-	implementation(libs.springBootStarterWeb)
-	testImplementation(libs.springBootStarterTest)
-	testRuntimeOnly(libs.junitPlatform)
-	testImplementation(libs.junitJupiter)
-	testImplementation(libs.assertj)
+    implementation ("org.springframework.boot:spring-boot-starter")
+    implementation ("org.springframework.boot:spring-boot-starter-web")
+    compileOnly ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.springBootStarterWeb)
+    testImplementation(libs.springBootStarterTest)
+    testRuntimeOnly(libs.junitPlatform)
+    testImplementation(libs.junitJupiter)
+    testImplementation(libs.assertj)
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.register<Zip>("zipJavaDoc") {
@@ -59,6 +63,27 @@ tasks.register<Zip>("zipJavaDoc") {
     from("build/docs/javadoc") // Исходная папка для упаковки
     archiveFileName.set("javadoc.zip") // Имя создаваемого архива
     destinationDirectory.set(layout.buildDirectory.dir("archives")) // Директория, куда будет сохранен архив
+}
+
+tasks.register("checkJarSize") {
+    group = "verification"
+    description = "Checks the size of the generated JAR file."
+
+    dependsOn("jar")
+
+    doLast {
+        val jarFile = file("build/libs/${project.name}-${project.version}.jar")
+        if (jarFile.exists()) {
+            val sizeInMB = jarFile.length() / (1024 * 1024)
+            if (sizeInMB > 5) {
+                println("WARNING: JAR file exceeds the size limit of 5 MB. Current size: ${sizeInMB} MB")
+            } else {
+                println("JAR file is within the acceptable size limit. Current size: ${sizeInMB} MB")
+            }
+        } else {
+            println("JAR file not found. Please make sure the build process completed successfully.")
+        }
+    }
 }
 
 tasks.spotbugsMain {
---
+++ buildSrc/DependencyVersions.kt
@@ -0,0 +1,3 @@
+object DependencyVersions {
+    const val COMMONS_LANG = "3.12.0"
+}
